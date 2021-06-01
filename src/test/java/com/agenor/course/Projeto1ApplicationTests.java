81745e87052316f95564acc596a4fb0178775cd1
Agenor de Lima Bento <agenordelima@msn.com>
2021-06-01 17:05:46 -0300

    Criando projeto

---
+++ src/test/java/com/agenor/course/Projeto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenor.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
