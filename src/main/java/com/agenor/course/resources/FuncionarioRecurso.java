9cb21ab13f3c0f34fd8f79e452ee0a780971ea06
Agenor de Lima Bento <agenordelima@msn.com>
2021-06-07 15:09:03 -0300

    Camada de serviço e registro de componentes

--- src/main/java/com/agenor/course/resources/FuncionarioRecurso.java
+++ src/main/java/com/agenor/course/resources/FuncionarioRecurso.java
@@ -1,23 +1,35 @@
 package com.agenor.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.agenor.course.entities.Funcionario;
+import com.agenor.course.services.FuncionarioService;
 
 @RestController
 @RequestMapping(value = "/funcionarios")
 public class FuncionarioRecurso {
 	//FuncionárioRecurso: serve para disponibilizar recurso web correspondente a entidade Funcionário
 	
+	@Autowired
+	private FuncionarioService service;
+	
 	@GetMapping
-	public ResponseEntity<Funcionario> findAll() {
-		Funcionario f = new Funcionario(1L, "João", "Da Silva", "Diretor", "44444", "123456");
-		return ResponseEntity.ok().body(f);
-		// Retornar
-		
+	public ResponseEntity<List<Funcionario>> findAll() {
+		List<Funcionario> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Funcionario> findById(@PathVariable Long id) {
+		Funcionario obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
---
+++ src/main/java/com/agenor/course/services/FuncionarioService.java
@@ -0,0 +1,28 @@
+package com.agenor.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.agenor.course.entities.Funcionario;
+import com.agenor.course.repositories.FuncionarioRepository;
+
+@Service
+public class FuncionarioService {
+
+	@Autowired
+	private FuncionarioRepository repository;
+	
+	public List<Funcionario> findAll(){
+		return repository.findAll();
+		//Operação na camada de serviço que repassa a chamada para repository.findAll
+	}
+	
+	public Funcionario findById(Long id) {
+		Optional<Funcionario> obj = repository.findById(id);
+		return obj.get();
+		// Retornar objeto Funcionario que estiver dentro do Optional
+	}
+}
